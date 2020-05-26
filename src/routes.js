4646ab6a08a8798071a7bed9f397f1183e4d8188
gabrielrsp <gabrielrsp18@gmail.com>
2020-05-25 22:48:13 -0300

    create test route

--- src/routes.js
+++ src/routes.js
@@ -11,11 +11,12 @@ import validateToolStore from './app/validators/ToolStore';
 
 import authMiddleware from './app/middlewares/auth';
 
-
 const routes = new Router();
 
 routes.post('/users', validateUserStore, UserController.store);
 routes.post('/sessions', validateSessionStore, SessionController.store);
+
+routes.get('/', (req, res) => res.send('ok'));
 
 routes.use(authMiddleware);
 routes.put('/users', validateUserUpdate, UserController.update);
@@ -23,9 +24,6 @@ routes.put('/users', validateUserUpdate, UserController.update);
 routes.get('/tools', ToolController.index);
 routes.post('/tools', validateToolStore, ToolController.store);
 routes.delete('/tools/:id', ToolController.delete)
-
-
-
 
 export default routes;
 
