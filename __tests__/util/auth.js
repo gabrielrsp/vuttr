bfd6417fbe93bc02849cbec612025818fa088445
gabrielrsp <gabrielrsp18@gmail.com>
2020-05-27 01:49:33 -0300

    refactor auth file

--- __tests__/util/auth.js
+++ __tests__/util/auth.js
@@ -1,29 +1,28 @@
 
 import request from 'supertest';
 import app from '../../src/app';
-import truncate from '../util/truncate';
+
 import factory from '../factories';
 
-export default async () => {
-
-  await truncate();
+const token = async () => {
 
   const user = await factory.attrs('User');
   await request(app)
     .post('/users')
     .send(user);
 
-
   const response = await request(app)
-  .post('/sessions')
-  .send({
-    email: user.email,
-    password: user.password
-  });
+    .post('/sessions')
+    .send({
+      email: user.email,
+      password: user.password
+    });
 
-  return { token: response.body.token }; // save the token!
+  return { token: response.body.token };
 
-}
+};
+
+export default token;
 
 
 
