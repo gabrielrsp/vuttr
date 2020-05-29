04d8de9fb2d284cd6c79518afa64f7a01872bf48
gabrielrsp <gabrielrsp18@gmail.com>
2020-05-29 19:35:10 -0300

    create test route

--- src/routes.js
+++ src/routes.js
@@ -13,6 +13,8 @@ import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
 
+routes.get('/', (req, res) => res.send('ok'));
+
 routes.post('/users', validateUserStore, UserController.store);
 routes.post('/sessions', validateSessionStore, SessionController.store);
 
